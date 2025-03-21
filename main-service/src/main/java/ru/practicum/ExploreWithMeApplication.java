0e021becb2f7fca0a2fde6983f6770d34cf1771f
Artem Oleshev <sunlightpattern@yandex.ru>
2025-03-21 21:38:40 +0300

    feat: add main service

---
+++ main-service/src/main/java/ru/practicum/ExploreWithMeApplication.java
@@ -0,0 +1,10 @@
+package ru.practicum;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
