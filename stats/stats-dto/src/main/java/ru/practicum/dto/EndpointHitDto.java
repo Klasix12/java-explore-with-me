03555b2bcc68e0db767a7d13d9ee5373e1b4dc31
Artem Oleshev <sunlightpattern@yandex.ru>
2025-03-31 12:10:11 +0300

    fix: change @NotBlank to @NotNull

--- stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/dto/EndpointHitDto.java
@@ -2,6 +2,7 @@ package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import ru.practicum.util.DateFormat;
 
@@ -21,7 +22,7 @@ public class EndpointHitDto {
     @NotBlank
     private String ip;
 
-    @NotBlank
+    @NotNull
     @JsonFormat(pattern = DateFormat.DATE_TIME_FORMAT)
     private LocalDateTime timestamp;
 }
